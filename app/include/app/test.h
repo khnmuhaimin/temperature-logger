c20ba50b22a30c0f3175d7b4cfb7a94d3b861201
Muhaimin Khan <khnmuhaimin@gmail.com>
2025-11-29 03:10:26 +0200

    Bug fix

--- app/include/app/test.h
+++ app/include/app/test.h
@@ -1,9 +1,11 @@
 #ifndef TEST_H
 #define TEST_H
 
-#if CONFIG_ENABLE_TESTING
+#if CONFIG_BUILD_TEST_APP
+// disables static
 #define EXPOSE_FOR_TESTING
 #else
+// enables static
 #define EXPOSE_FOR_TESTING static
 #endif
 
